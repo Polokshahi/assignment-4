f356fd311aaf41b31bbb7bed274de98bf89f95a2
Polok Shahi <polokshahi2@gmail.com>
2026-02-08 04:49:27 +0600

    deploy issue solving

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import app from './app';
 
 const server = () => {
   const PORT = process.env.PORT || 7000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  
+  
+  if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
 };
 
 server();
+
+
+export default app;
